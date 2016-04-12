93180e87e79e9e88b54a7f91c03a51b5040ac7f8
Guillaume Le Neindre <guillaume.le.n@gmail.com>
2016-04-12 21:25:09 +0000

    HW3 Part2 step 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    if Movie.find_by_title(movie[title]) == nil
+      new_movie = Movie.create(movie)
+    end
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -17,6 +19,26 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
+Then(/^I should see the following movies: (.*)/) do |movie_list|
+  movie_list.split(',').each do |movie|
+    if page.respond_to? :should
+      page.should have_content(movie.strip)
+    else
+      assert page.has_content?(movie.strip)
+    end
+  end
+end
+
+Then(/^I should not see the following movies: (.*)/) do |movie_list|
+  movie_list.split(',').each do |movie|
+    if page.respond_to? :should
+      page.should have_no_content(movie.strip)
+    else
+      assert page.has_no_content?(movie.strip)
+    end
+  end
+end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
@@ -25,7 +47,13 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  rating_list.split(',').each do |rating|
+    if uncheck
+      uncheck("ratings_" + rating.strip)
+    else
+      check("ratings_" + rating.strip)
+    end
+  end
 end
 
 Then /I should see all the movies/ do
